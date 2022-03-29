d377bc1cf292eb74974885d922ad8da16f7397ac
Baiqing Lyu <baiqing@bu.edu>
2022-03-29 14:58:48 -0400

    Test OCR code?

--- src/ocr/extract_text.py
+++ src/ocr/extract_text.py
@@ -1,14 +1,16 @@
+from pdf2image import convert_from_path
+from tqdm import tqdm
 import easyocr
+import numpy as np
 
-def extract_text(img_path):
+def extract_text(img_path, ocr_engine):
     """
     Extract text from an image using OCR.
     """
     # Load the OCR engine
-    ocr = easyocr.Reader(['en'])
 
     # Extract the text
-    text = ocr.readtext(img_path)
+    text = ocr_engine.readtext(img_path)
 
     result = []
 
@@ -19,3 +21,25 @@ def extract_text(img_path):
 
     # Return the text
     return result
+
+def pdf_to_text(pdf_path):
+    """
+    Convert a pdf to a list of text.
+    """
+    # Convert pdf to images
+    images = convert_from_path(pdf_path)
+
+    # Convert them to numpy arrays
+    np_arrays = []
+    for image in images:
+        np_arrays.append(np.asarray(image))
+    
+    print("Converted PDF to images.")
+    
+    result = []
+    ocr = easyocr.Reader(['en'])
+    # Extract the text
+    for i in tqdm(range(len(np_arrays))):
+        result.append(extract_text(np_arrays[i], ocr))
+
+print(pdf_to_text('example.pdf'))
