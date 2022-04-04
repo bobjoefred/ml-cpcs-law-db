178e32ac4fbf8f4228aae29db006308e543b81e8
nicholasjhall <njhall@bu.edu>
2022-04-04 14:54:20 -0400

    Change input for get_suit_fields

--- src/suit_extraction/field_extraction.py
+++ src/suit_extraction/field_extraction.py
@@ -1,18 +1,8 @@
 import sys
 sys.path.append("..")
-
-import fitz # pymupdf
-
-# Need to install tesseract in order to use OCR to extract text
-# https://pymupdf.readthedocs.io/en/latest/installation.html
-
-import nltk
-from nltk.tokenize import word_tokenize
-nltk.download('punkt')
 import re
 import os
 from os import walk, path
-from ocr import extract_text
 
  
 '''
@@ -31,46 +21,15 @@ notes - notes - https://drive.google.com/drive/folders/191GkC8uPO4wJmrypBJVyRFYI
 '''
 
 
-# input: directory path containing files for a single civil suit
-# output: dictionary with all data fields for the civil suit
-def get_suit_fields(directory):
-
-  # standardize path name
-  directory = path.join(directory, '')
-
-  # get all files in directory recursively
-  _, _, filenames = next(walk(directory), (None, None, []))
-
-  # print(filenames)
-
-  # search through filenames to find relevant complaint and order documents
-  complaint_filenames = [fn for fn in filenames if 'Complaint' in fn]
-  complaint_filename = complaint_filenames[0]
-  order_filenames = [fn for fn in filenames if 'Order' in fn or 'Judgment' in fn]
-  order_filename = order_filenames[-1]
-  print('Complaint: ' + complaint_filename + '\nOrder: ' + order_filename)
-
-
-  # use OCR for complaint document, which is scanned
-  # the text is organized as a list of strings, each one line of the document
-  complaint_lines = extract_text.pdf_to_text(directory + complaint_filename)
-  # print(complaint_lines)
-
-  # use simple text extraction for order document, which is digitally created
-  order_tokens = []
-  with fitz.open(directory + order_filename) as doc:
-    for page in doc:
-      page_text = page.get_text().lower()
-      page_tokens = word_tokenize(page_text)
-      order_tokens += page_tokens
-
+# takes in a list of lines from complaint doc and a list of tokens from order doc
+# returns dictionary of extracted fields
+def get_suit_fields(complaint_lines, order_tokens):
   # extract fields
   docket_num = extract_docket_num(order_tokens)
 
   officers = extract_officers(complaint_lines)
 
-  fields = { 'docket_num': docket_num, 'officers': officers }
-  print(fields)
+  fields = { 'Docket Number': docket_num, 'Officer(s)': officers }
 
   return fields
 
@@ -81,11 +40,15 @@ def extract_docket_num(tokens):
   # docket number e.g. 1:16-cv-11865-WGY or 1184CV00961
   docket_num_regex = re.compile('[0-9]:?[0-9]*-?cv.*')
   docket_num = list(filter(docket_num_regex.match, tokens))[0].upper()
+
   return docket_num
 
 # pass in complaint lines, returns list of officers
 def extract_officers(lines):
   officers_regex = re.compile('Defendant ([a-zA-Z\']{3,40})(?:\s[A-Z].)?\s([a-zA-Z\']{3,40}) (is|was)')
   officers = [m.group(1) + ' ' + m.group(2) for m in (officers_regex.match(str(line)) for line in lines) if m]
+
+  # remove duplicates
   officers = list(set(officers))
+
   return officers
